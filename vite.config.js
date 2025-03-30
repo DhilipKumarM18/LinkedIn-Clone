fb3a3d42fd6b956bfc8adeac5969dbef35a0c55a
DhilipKumarM18 <157344636+DhilipKumarM18@users.noreply.github.com>
2025-03-30 19:22:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true, // ✅ Helps with SPA routing issues
+    historyApiFallback: true, 
   },
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/_tests_/setupTests.js",
     transformMode: {
-      web: [/.[jt]sx?$/], // ✅ Ensures JS & JSX parsing
+      web: [/.[jt]sx?$/],
     },
   },
 });
